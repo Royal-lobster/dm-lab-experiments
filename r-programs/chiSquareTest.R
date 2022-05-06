e9eda8647f1aa70cd1af4f03041ea71783590e17
Srujan Gurram <srujangs8@gmail.com>
2022-05-06 17:17:24 +0530

    added description and output to chiSquaredTest

--- r-programs/chiSquareTest.R
+++ r-programs/chiSquareTest.R
@@ -1,10 +1,31 @@
-iris$size <- ifelse(
-    iris$Sepal.Length < median(iris$Sepal.Length),
-    "small", "big"
-)
+# Correlation is done for ordinal values where
+# as for nominal values, its not possible to compute
+# so we use tests. The chi-square test of independence
+# evaluates whether there is an association between the
+# categories of the two variables.
 
-data <- table(iris$Species, iris$size)
-print(data)
+library(MASS)
 
-result <- chisq.test(table(iris$Species, iris$size))
-print(result)
+stu_data <- data.frame(survey$Smoke, survey$Exer)
+stu_data <- table(survey$Smoke, survey$Exer)
+print(stu_data)
+
+print(chisq.test(stu_data))
+
+# OUTPUT =>
+#         Freq None Some
+#   Heavy    7    1    3
+#   Never   87   18   84
+#   Occas   12    3    4
+#   Regul    9    1    7
+#
+#         Pearson's Chi-squared test
+# data:  stu_data
+# X-squared = 5.4885, df = 6, p-value = 0.4828
+#
+# SUMMARY =>
+#
+# So, since  p-value 0.4828 is greater than the .05,
+# we conclude that the smoking habit is independent of the
+# exercise level of the student and hence there is a weak or
+# no correlation between the two variables.
